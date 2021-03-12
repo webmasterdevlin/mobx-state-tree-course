f230fd20d8157d132c5fa44c904805617857c8a8
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-03-12 22:29:34 +0100

    updated

--- src/components/TotalOfCharacters.tsx
+++ src/components/TotalOfCharacters.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { observer } from "mobx-react-lite";
 
 type Props = {
   total: number;
   role: string;
 };
 
-const TotalOfCharacters = observer(({ total, role }: Props) => (
+const TotalOfCharacters = ({ total, role }: Props) => (
   <span role={role} style={{ color: "cyan", margin: "0 1rem" }}>
     {total}
   </span>
-));
+);
 
 export default TotalOfCharacters;
